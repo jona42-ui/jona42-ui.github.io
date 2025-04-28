dae16270d186b86116b0f7c17224adaf46a67920
jona42-ui <jonathanthembo123@gmail.com>
2025-04-28 21:13:53 +0300

    Update UI styles and add hero section with profile image

--- assets/js/main.js
+++ assets/js/main.js
@@ -174,3 +174,71 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   }
 });
+
+// Text rotation animation
+var TxtRotate = function(el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = '';
+  this.tick();
+  this.isDeleting = false;
+};
+
+TxtRotate.prototype.tick = function() {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+
+  if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+  var that = this;
+  var delta = 200 - Math.random() * 100;
+
+  if (this.isDeleting) { delta /= 2; }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+  }
+
+  setTimeout(function() {
+    that.tick();
+  }, delta);
+};
+
+// Initialize text rotation
+document.addEventListener('DOMContentLoaded', function() {
+  var elements = document.getElementsByClassName('txt-rotate');
+  for (var i=0; i<elements.length; i++) {
+    var toRotate = elements[i].getAttribute('data-rotate');
+    var period = elements[i].getAttribute('data-period');
+    if (toRotate) {
+      new TxtRotate(elements[i], JSON.parse(toRotate), period);
+    }
+  }
+});
+
+// Offline detection
+window.addEventListener('online', function() {
+  document.querySelector('.offline-indicator')?.remove();
+});
+
+window.addEventListener('offline', function() {
+  if (!document.querySelector('.offline-indicator')) {
+    const indicator = document.createElement('div');
+    indicator.className = 'offline-indicator';
+    indicator.innerHTML = '<div class="offline-message">You are currently offline</div>';
+    document.body.prepend(indicator);
+  }
+});
